a555a77294967817456f92dfcf76c49c18f511a0
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-02-03 16:22:14 -0300

    add_framentview

--- app/src/main/java/com/example/space_informatica/ui/client/ManutenActivity.kt
+++ app/src/main/java/com/example/space_informatica/ui/client/ManutenActivity.kt
@@ -1,28 +1,50 @@
 package com.example.space_informatica.ui.client
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.space_informatica.R
 import com.example.space_informatica.databinding.ActivityManutenBinding
+import com.example.space_informatica.ui.client.fragments.FragmentManuntenProgramada
+import com.example.space_informatica.ui.client.fragments.FragmentManutenSolicitadas
 
 class ManutenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityManutenBinding
+    private lateinit var btnProgramadas: Button
+    private lateinit var btnSolicitadas: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        
+
         binding = ActivityManutenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        btnProgramadas = findViewById(R.id.btnProgramadas)
+        btnSolicitadas = findViewById(R.id.btnSolicitadas)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val fragmentManutenSolicitadas = FragmentManutenSolicitadas()
+        val fragmentManutenProgramadas = FragmentManuntenProgramada()
+
+
+        btnProgramadas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_conteudo, fragmentManutenProgramadas)
+                .commit()
         }
+
+        btnSolicitadas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_conteudo, fragmentManutenSolicitadas)
+                .commit()
+        }
+
+
     }
 }
