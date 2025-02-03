2f44882184f17d8b17c08c3c1a1f6923abcede5a
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-02-03 17:04:34 -0300

    optimize imports

--- app/src/main/java/com/example/space_informatica/ui/client/ManutenActivity.kt
+++ app/src/main/java/com/example/space_informatica/ui/client/ManutenActivity.kt
@@ -1,11 +1,8 @@
 package com.example.space_informatica.ui.client
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.space_informatica.R
 import com.example.space_informatica.databinding.ActivityManutenBinding
 import com.example.space_informatica.ui.client.fragments.FragmentManuntenProgramada
