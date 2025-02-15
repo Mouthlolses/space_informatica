1f3ee22bf6da33b63d6bae637ac58e8365ce67e6
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-02-15 13:34:32 -0300

    add_fragments_screens

--- app/src/main/java/com/example/space_informatica/ui/client/ManutenActivity.kt
+++ app/src/main/java/com/example/space_informatica/ui/client/ManutenActivity.kt
@@ -1,23 +1,45 @@
 package com.example.space_informatica.ui.client
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.space_informatica.R
 import com.example.space_informatica.databinding.ActivityManutenBinding
+import com.example.space_informatica.ui.client.fragments.RequestFragment
+import com.example.space_informatica.ui.client.fragments.ScheduledFragment
 
 class ManutenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityManutenBinding
+    private lateinit var btnScheduled: Button
+    private lateinit var btnRequest: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-
         binding = ActivityManutenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        btnScheduled = binding.buttonProgramada
+        btnRequest = binding.buttonSolicitada
+
+
+        btnScheduled.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_content, ScheduledFragment())
+                .commit()
+        }
+
+        btnRequest.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_content, RequestFragment())
+                .commit()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
