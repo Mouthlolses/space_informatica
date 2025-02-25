aced72e8760e0a7753f24829c42c5b3c50046ad3
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-02-25 16:12:20 -0300

    logic_screenlogin

--- app/src/main/java/com/example/space_informatica/ui/screens_initial/LoginActivity.kt
+++ app/src/main/java/com/example/space_informatica/ui/screens_initial/LoginActivity.kt
@@ -2,7 +2,6 @@ package com.example.space_informatica.ui.screens_initial
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,26 +12,50 @@ import com.example.space_informatica.ui.client.ClientActivity
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var btnAcess: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        btnAcess = binding.btnAcess
-        btnAcess.setOnClickListener {
-            intent = Intent(this, ClientActivity::class.java)
-            (startActivity(intent))
-
-        }
-
+        setupListeners()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    private fun setupListeners() {
+        binding.btnAcess.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.Password.text.toString()
+
+            if (validateEntryLogin(email, password)) {
+                entry()
+            }
+        }
+    }
+
+    private fun entry() {
+        val intent = Intent(this, ClientActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun validateEntryLogin(lEmail: String, lPassword: String): Boolean {
+
+        binding.loginEmail.error = null
+        binding.loginPassword.error = null
+
+        if (lEmail.isEmpty()) {
+            binding.loginEmail.error = "Digite seu email"
+            return false
+        } else if (lPassword.isEmpty()) {
+            binding.loginPassword.error = "Digite sua senha"
+            return false
+        }
+        return true
+    }
+
 }
