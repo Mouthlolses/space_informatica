2770df9a14da92db59e3ebd095ed37a5e7c41703
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-02-26 15:55:15 -0300

    database run

--- app/src/main/java/com/example/space_informatica/data/database/Database.kt
+++ app/src/main/java/com/example/space_informatica/data/database/Database.kt
@@ -1,8 +1,10 @@
 package com.example.space_informatica.data.database
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.space_informatica.model.Usuarios
 
-
+@Database(entities = [Usuarios::class], version = 1, exportSchema = false)
 abstract class Database : RoomDatabase() {
 
 
