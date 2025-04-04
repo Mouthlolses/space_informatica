846de2d0560e49f76e1d53c1b37f3ad11645206b
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-04-04 18:42:13 -0300

    reformat_loginActivity

--- app/src/main/java/com/example/space_informatica/ui/screens_initial/LoginActivity.kt
+++ app/src/main/java/com/example/space_informatica/ui/screens_initial/LoginActivity.kt
@@ -43,7 +43,6 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(binding.root)
-        //setupListeners()
         binding.btnRegister.setOnClickListener {
             registerUser()
         }
@@ -90,31 +89,5 @@ class LoginActivity : AppCompatActivity() {
         val intent = Intent(this, ClientActivity::class.java)
         startActivity(intent)
     }
-
-    private fun setupListeners() {
-        binding.btnAcess.setOnClickListener {
-            val email = binding.email.text.toString()
-            val password = binding.Password.text.toString()
-
-            if (validateEntryLogin(email, password)) {
-                entry()
-            }
-        }
-    }
-
-    private fun validateEntryLogin(lEmail: String, lPassword: String): Boolean {
-
-        binding.loginEmail.error = null
-        binding.loginPassword.error = null
-
-        if (lEmail.isEmpty()) {
-            binding.loginEmail.error = "Digite seu email"
-            return false
-        } else if (lPassword.isEmpty()) {
-            binding.loginPassword.error = "Digite sua senha"
-            return false
-        }
-        return true
-    }
-
 }
+
